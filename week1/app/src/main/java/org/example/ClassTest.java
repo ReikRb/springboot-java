c31f71d43a925360e84135283b3a8a7948c4f177
Unai <reikrb@gmail.com>
2025-02-16 13:55:26 +0100

    Add scanner

--- week1/app/src/main/java/org/example/ClassTest.java
+++ week1/app/src/main/java/org/example/ClassTest.java
@@ -1,6 +1,14 @@
 package org.example;
+
+import java.util.Scanner;
+
 public class ClassTest {
     public static void main(String[] args) {
         System.out.println("Hola, Java");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Insert name");
+        String name = scanner.nextLine();
+        System.out.println("Hello, " + name);
+        scanner.close();
     }
 }
